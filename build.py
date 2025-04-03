1c151c10053c239c9250b8f3146b018b088f5a75
yuyang <502950715@qq.com>
2025-04-03 16:22:57 +0800

    修改打包方式，提高打包后启动速度

--- build.py
+++ build.py
@@ -3,24 +3,24 @@ import PyInstaller.__main__
 import shutil
 import os
 
-BUILD_PATH = "eve_tool"
+BUILD_PATH = "dist/eve_tool"
 
 # 清理历史构建
-if os.path.exists(BUILD_PATH):
-    shutil.rmtree(BUILD_PATH)
+if os.path.exists("dist"):
+    shutil.rmtree("dist")
 if os.path.exists("build"):
     shutil.rmtree("build")
 
 PyInstaller.__main__.run(
     [
         "main.py",
-        "--onefile",
+        "--onedir",
         "--windowed",
-        "--distpath=./eve_tool",
+        "--distpath=./dist",
         "--name=eve_tool",
         "--icon=./imgs/icon.ico",
         "--hidden-import=pyautogui",
-        "--hidden-import=playsound",
+        "--strip",  # 去除调试信息
     ]
 )
 
