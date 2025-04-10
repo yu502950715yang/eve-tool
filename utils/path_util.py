56f51c944d12ed2d78e901385fbab6b5becd33fd
yuyang <502950715@qq.com>
2025-04-10 10:16:56 +0800

    优化

--- utils/path_util.py
+++ utils/path_util.py
@@ -1,7 +1,5 @@
-#  预警图片路径
 import os
 import sys
-
 
 RESOURCE_PATHS = {
     "alert_img": {"dev": "../imgs/alert", "prod": "imgs/alert"},
@@ -9,30 +7,31 @@ RESOURCE_PATHS = {
     "config": {"dev": "../config", "prod": "config"},
 }
 
-def _get_base_dir():
+def is_frozen() -> bool:
+    """判断程序是否是打包后的运行环境"""
+    return getattr(sys, "frozen", False)
+
+def _get_base_dir() -> str:
+    """获取基础目录"""
     return os.path.dirname(sys.executable) if is_frozen() else \
            os.path.dirname(os.path.abspath(__file__))
 
-def get_resource_path(resource_key):
+def get_resource_path(resource_key: str) -> str:
     """通用资源路径获取函数"""
     base_dir = _get_base_dir()
     paths = RESOURCE_PATHS[resource_key]
     path = paths["prod"] if is_frozen() else paths["dev"]
     return os.path.normpath(os.path.join(base_dir, path))
 
-def get_alert_img_path():
-    """获取预警图片路径"""
-    return get_resource_path("alert_img")
-
-def get_alert_sound_path():
-    """获取预警声音路径"""
-    return get_resource_path("alert_sound")
-
-def get_config_path(relative_path):
+def get_config_path(relative_path: str) -> str:
     """获取配置文件路径"""
     config_base_path = get_resource_path("config")
     return os.path.normpath(os.path.join(config_base_path, relative_path))
 
-def is_frozen():
-    """判断程序是否是打包后的运行环境"""
-    return getattr(sys, "frozen", False)
+def get_alert_img_path() -> str:
+    """获取预警图片路径"""
+    return get_resource_path("alert_img")
+
+def get_alert_sound_path() -> str:
+    """获取预警声音路径"""
+    return get_resource_path("alert_sound")
